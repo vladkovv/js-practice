81286a55cfe1c58b845db1f13f61f40d86f85c6d
Vladislav Kovalyok <vlad.kovalyok.99@gmail.com>
2020-02-24 16:16:59 +0300

    developed delay function

--- src/main.js
+++ src/main.js
@@ -1,29 +1,10 @@
-class Universe {
-    constructor() {
-        if (Universe.instance instanceof Universe) {
-            return Universe.instance;
-        }
+async function delay(del) {
+    let promise = new Promise((resolve) => {
+        setTimeout(() => resolve(del), del)
+    })
 
-        this.size = 100;
-        Universe.instance = this;
-        return this;
-    }
-
-    getSize() {
-        return this.size;
-    }
-
-    setSize(number) {
-        return this.size = number;
-    }
+    let value = await promise;
+    return value;
 }
 
-let a = new Universe();
-let b = new Universe();
-
-console.log(a.getSize(), b.getSize());
-a.setSize(200);
-console.log(a.getSize(), b.getSize());
-b.setSize(300);
-console.log(a.getSize(), b.getSize());
-console.log(a === b);
+delay(1000).then(value => console.log(`Done with ${value}`));
