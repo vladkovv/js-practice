cde347d575b5af2c4eb352d09d8113689a4777cf
Vladislav Kovalyok <vlad.kovalyok.99@gmail.com>
2020-02-25 17:14:38 +0300

    some fixes

--- src/getUser.js
+++ src/getUser.js
@@ -1,16 +1,14 @@
-fetch('http://www.json-generator.com/api/json/get/cfQCylRjuG')
-    .then(response => {
-        response.json()
+let firstResponse = fetch('http://www.json-generator.com/api/json/get/cfQCylRjuG');
+let secondResponse = fetch('http://www.json-generator.com/api/json/get/cfVGucaXPC');
+firstResponse
+        .then(response => response.json())
         .then(json => {
-            if (json.getUsersData) {
-            fetch('http://www.json-generator.com/api/json/get/cfVGucaXPC')
-            .then(response => {
-             response.json()
-            .then(json => {
-             console.log(json)
-             })
-        })
+        if (json.getUsersData) {
+            secondResponse
+            .then(response => response.json()
+            .then(json => console.log(json))
+        )
         }
     })
- })
+
  
