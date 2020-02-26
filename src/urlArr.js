9af6db1622a07efc98d78990ed26c819c85fa5bd
Vladislav Kovalyok <vlad.kovalyok.99@gmail.com>
2020-02-26 14:02:34 +0300

    receiving data from array of urls

---
+++ src/urlArr.js
@@ -0,0 +1,15 @@
+const url = [
+    'http://www.json-generator.com/api/json/get/cevhxOsZnS',
+    'http://www.json-generator.com/api/json/get/cguaPsRxAi',
+    'http://www.json-generator.com/api/json/get/cfDZdmxnDm',
+    'http://www.json-generator.com/api/json/get/cfkrfOjrfS',
+    'http://www.json-generator.com/api/json/get/ceQMMKpidK'
+];
+
+let fetchData = url.map(url => fetch(url)
+.then(data => data.json()))
+
+Promise.all(fetchData)
+.then(data => console.log(data));
+
+
