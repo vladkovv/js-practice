0342ea0d09717230fd1a1a33ccce61015691bc30
Vladislav Kovalyok <vlad.kovalyok.99@gmail.com>
2020-02-26 12:21:53 +0300

    another little fixes

--- src/getUser.js
+++ src/getUser.js
@@ -1,14 +1,16 @@
-let firstResponse = fetch('http://www.json-generator.com/api/json/get/cfQCylRjuG');
-let secondResponse = fetch('http://www.json-generator.com/api/json/get/cfVGucaXPC');
+const firstResponse = fetch('http://www.json-generator.com/api/json/get/cfQCylRjuG');
+let secondResponse = null;
+
 firstResponse
         .then(response => response.json())
         .then(json => {
         if (json.getUsersData) {
-            secondResponse
-            .then(response => response.json()
-            .then(json => console.log(json))
-        )
+            secondResponse = fetch('http://www.json-generator.com/api/json/get/cfVGucaXPC');
         }
+ secondResponse
+        .then(response => response.json()
+        .then(json => console.log(json))       
+    )
     })
 
  
