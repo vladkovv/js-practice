bdaab8d7be9895d85e65fecbc9cefef2316b085b
Vladislav Kovalyok <vlad.kovalyok.99@gmail.com>
2020-03-04 16:42:00 +0300

    added myBind function

--- src/polyfills.js
+++ src/polyfills.js
@@ -1,24 +1,34 @@
 function showFullName(firstPart, lastPart) {
-    console.log( this[firstPart] + " " + this[lastPart] );
-  }
-  
-  const user = {
+    console.log(this[firstPart] + " " + this[lastPart]);
+}
+
+const user = {
     firstName: "Василий",
     lastName: "Петров",
     patronym: 'Иванович'
-  };
+};
 
-Function.prototype.myCall = function(context, ...args) {
+Function.prototype.myCall = function (context, ...args) {
     return this.apply(context, args)
 }
 
 
-Function.prototype.myApply = function(context, args) {
+Function.prototype.myApply = function (context, args) {
     return this.call(context, ...args)
 }
+
+Function.prototype.myBind = function (context, ...args) {
+    _this = this;
+    return function () {
+        return _this.apply(context, args)
+    }
+}
+
 showFullName.myCall(user, 'firstName', 'lastName');
 showFullName.myCall(user, 'firstName', 'patronym');
-console.log('===================================');
 
 showFullName.myApply(user, ['firstName', 'lastName']);
 showFullName.myApply(user, ['firstName', 'patronym']);
+
+const bindedFunc = showFullName.myBind(user, 'firstName', 'patronym');
+bindedFunc();
