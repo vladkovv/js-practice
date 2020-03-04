b10075e3fc21a877509e8360bedda28b6839fad1
Vladislav Kovalyok <vlad.kovalyok.99@gmail.com>
2020-03-04 16:11:24 +0300

    added myApply function

--- src/polyfills.js
+++ src/polyfills.js
@@ -12,5 +12,13 @@ Function.prototype.myCall = function(context, ...args) {
     return this.apply(context, args)
 }
 
-showFullName.myCall(user, 'firstName', 'lastName')
-showFullName.myCall(user, 'firstName', 'patronym')
+
+Function.prototype.myApply = function(context, args) {
+    return this.call(context, ...args)
+}
+showFullName.myCall(user, 'firstName', 'lastName');
+showFullName.myCall(user, 'firstName', 'patronym');
+console.log('===================================');
+
+showFullName.myApply(user, ['firstName', 'lastName']);
+showFullName.myApply(user, ['firstName', 'patronym']);
