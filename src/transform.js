f8c04b2061f2ea8174f602d91f2bfb030f10409c
Vladislav Kovalyok <vlad.kovalyok.99@gmail.com>
2020-03-05 16:03:43 +0300

    fixes after review

--- src/transform.js
+++ src/transform.js
@@ -1,13 +1,6 @@
 function transform(str) {
-    
-    const dot = '.';
-    const arr = str.split(dot);
-    const result = arr.reduceRight((acc, curr, index, arr) => {
 
-       return index < arr.length - 1 ?  {[curr] : acc} : {[curr] : null}
+return str.split('.').reduceRight((acc, curr) => ({[curr] : acc}), {})
 
-    }, {})
-
-    return result;
 }
 console.log(transform('a.b.c.d'));
